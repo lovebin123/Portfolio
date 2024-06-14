8d3b589a1138275b08bb5c872b51fcafef1a4f06
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2024-06-14 20:28:48 +0530

    ihih

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,22 @@
 import { Flex, Image,Text } from '@chakra-ui/react'
 import React from 'react'
+import Typewriter from 'typewriter-effect';
+
 import me from '../assets/me2.jpg'
 function Hero() {
   return (
-    <Flex gap={3} width={'98vw'} direction={'column'}   justifyContent={'center'} height={'full'} alignItems={'center'} zIndex={20} position={'relative'} backdropFilter={'blur(4px)'} bgGradient={'linear(to-bl, rgb(41 37 36),rgb(112, 128, 144))'} rounded={'4rem'}>
+    <Flex gap={6} width={'98vw'} direction={'column'}   justifyContent={'center'} height={'full'} alignItems={'center'} zIndex={20} position={'relative'} backdropFilter={'blur(4px)'} bgGradient={'linear(to-bl,rgb(30 41 59),rgb(41 37 36))'} rounded={'4rem'}>
+  <Flex opacity={1} direction={'column'} justifyContent={'center'} alignItems={'center'}>
    <Image objectFit={'cover'} src={me} w={64} height={64} rounded={'4rem'}/>
     <Text fontSize={'4xl'} fontStyle={'light'} color={'white'} fontWeight={'bold'}>LOVEBIN JOSEPH ROBIN</Text>
-    <Text color={'blue'}></Text>
+    <Text brightness={'40%'} contrast={'0%'} color={'#60a5fa'} letterSpacing={'0.025em'} fontSize={'2xl'} textAlign={'match-parent'}>
+       <Typewriter options={{
+        strings:['Tech Enthusiast','Software Developer'],
+        autoStart:true,
+        loop:true
+       }}/>
+    </Text>
+    </Flex>
     </Flex>
   )
 }
