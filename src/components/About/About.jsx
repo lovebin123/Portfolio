d2fba00feb84c2aceb7884fd728faa603b2f2942
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2024-06-17 22:45:22 +0530

    changeaa

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ function About() {
 
 
         <Flex justifyContent={'space-between'} px={20}>
-        <Flex w={'30rem'} h={100}>
+        <Flex w={'30rem'} h={100} justifyContent={'space-between'}>
           <Text  color={'white'} fontStyle={'light'} lineHeight={1.625} fontSize={'xl'}>
           Growing up in {' '}<Link color={'#60a5fa'} isExternal href='https://en.wikipedia.org/wiki/Kottayam' style={{textDecoration:'none', display:'inline-flex',alignItems:'center'}}>Kottayam<Image src={share} height={5}/> </Link>, I was always fascinated by computers. As a kid, I loved figuring out how they worked and exploring their capabilities. This curiosity only grew stronger as I got older, leading me to pursue a degree in computer science. I honed my skills in programming and software development, working on various projects that tested my creativity and problem-solving skills. <br></br><br></br>
           Now, I'm driven to use my expertise to create innovative solutions and make a meaningful impact in the tech world. I'm passionate about collaborating with others to bring new ideas to life. In my free time, I enjoy experimenting with new technologies and learning about the latest advancements in the field. My ultimate goal is to make a lasting difference through technology
