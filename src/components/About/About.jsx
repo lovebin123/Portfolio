bf527de96ca558efb71ee1130c885307663553fc
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2024-06-19 15:40:21 +0530

    changed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,42 +1,49 @@
-import { Avatar, Flex,Image,Link,Text,filter,keyframes } from '@chakra-ui/react'
-import React, { useEffect, useRef } from 'react'
-import './About.css'
-import gameboy from '../../assets/gameboy3.png'
-import share from '../../assets/share.png'
-import { useAnimation, useInView,motion, useScroll, AnimatePresence } from 'framer-motion'
-function About() {
-  const AboutRef=useRef(null)
-  const AboutInView=useInView(AboutRef,{once:false})
-const AboutControls=useAnimation()
-useEffect(() => {
-  if (AboutInView) {
-    AboutControls.start('visible');
-  }
+import { Avatar, Flex, Image, Link, Text } from '@chakra-ui/react';
+import React, { useEffect, useRef } from 'react';
+import './About.css';
+import gameboy from '../../assets/gameboy3.png';
+import share from '../../assets/share.png';
+import { useAnimation, useInView, motion, useScroll, AnimatePresence, useTransform } from 'framer-motion';
 
-}, [AboutInView]);
+function About() {
+  const AboutRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    container: AboutRef,
+  });
+  const y = useTransform(scrollYProgress, [1,0.75], [450, -480]);
+  const AboutInView = useInView(AboutRef, { once: false });
+  const AboutControls = useAnimation();
+
+  useEffect(() => {
+    if (AboutInView) {
+      AboutControls.start('visible');
+    }
+  }, [AboutInView]);
 
   return (
-    <Flex w={'full'} h={'full'} direction={'column'} gap={32}  justifyContent={'space-between'}>
-
-
-        <Flex justifyContent={'space-between'} px={20}>
+    <Flex w={'full'} h={'full'} direction={'column'} gap={32} justifyContent={'space-between'} ref={AboutRef}>
+      <Flex justifyContent={'space-between'} px={20}>
         <Flex w={'30rem'} h={100}>
-          <Text  color={'white'} fontStyle={'light'} lineHeight={1.625} fontSize={'xl'}>
-          Growing up in {' '}<Link color={'#60a5fa'} isExternal href='https://en.wikipedia.org/wiki/Kottayam' style={{textDecoration:'none', display:'inline-flex',alignItems:'center'}}>Kottayam<Image src={share} height={5}/> </Link>, I was always fascinated by computers. As a kid, I loved figuring out how they worked and exploring their capabilities. This curiosity only grew stronger as I got older, leading me to pursue a degree in computer science. I honed my skills in programming and software development, working on various projects that tested my creativity and problem-solving skills. <br></br><br></br>
-          Now, I'm driven to use my expertise to create innovative solutions and make a meaningful impact in the tech world. I'm passionate about collaborating with others to bring new ideas to life. In my free time, I enjoy experimenting with new technologies and learning about the latest advancements in the field. My ultimate goal is to make a lasting difference through technology
+          <Text color={'white'} fontStyle={'light'} lineHeight={1.625} fontSize={'xl'}>
+            Growing up in {' '}
+            <Link color={'#60a5fa'} isExternal href='https://en.wikipedia.org/wiki/Kottayam' style={{ textDecoration: 'none', display: 'inline-flex', alignItems: 'center' }}>
+              Kottayam<Image src={share} height={5} />
+            </Link>, I was always fascinated by computers. As a kid, I loved figuring out how they worked and exploring their capabilities. This curiosity only grew stronger as I got older, leading me to pursue a degree in computer science. I honed my skills in programming and software development, working on various projects that tested my creativity and problem-solving skills.
+            <br></br><br></br>
+            Now, I'm driven to use my expertise to create innovative solutions and make a meaningful impact in the tech world. I'm passionate about collaborating with others to bring new ideas to life. In my free time, I enjoy experimenting with new technologies and learning about the latest advancements in the field. My ultimate goal is to make a lasting difference through technology.
           </Text>
         </Flex>
-        <Flex  overflow={'hidden'} position={'relative'}   justifyContent={'flex-end'} >
-        <AnimatePresence>
-        <motion.div ref={AboutRef}  variants={{"hidden":{y:-200},"visible":{y:0}}} transition={{delay:1.2,duration:0.8}} initial="hidden" exit={{y:-100}}  animate={AboutControls}>
-      <Image  filter={"brightness(100%) "} className='gameboy'  w={'50rem'} src={gameboy} position={'relative'} left={80}></Image>
-      </motion.div>
-      </AnimatePresence>
-      <Text as={'sub'} color={'gray.500'}  transform={'rotate(90deg) translateY(270px)'}  >20's kids Nostalgia</Text>
-      </Flex>
+        <Flex overflow={'hidden'} position={'relative'} justifyContent={'flex-end'}>
+          <AnimatePresence>
+            <motion.div style={{ y }} variants={{ hidden: { y: -200 }, visible: { y: 0 } }} transition={{ delay: 0.5, duration: 0.8,ease:'linear' }} initial="hidden" exit={{ y: -100 }} animate={AboutControls}>
+              <Image filter={"brightness(100%)"} className='gameboy' w={'50rem'} src={gameboy} position={'relative'} left={80} />
+            </motion.div>
+          </AnimatePresence>
+          <Text as={'sub'} color={'gray.500'} transform={'rotate(90deg) translateY(270px)'}>20's kids Nostalgia</Text>
         </Flex>
+      </Flex>
     </Flex>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -6,9 +6,8 @@ import me from '../../assets/me2.jpg'
 function Landing() {
   return (
     <Flex
-     gap={6} width={'96vw'}  direction={'column'}   justifyContent={'center'} height={'95vh'}  zIndex={20} position={'relative'} backdropFilter={'blur(4px)'} bgGradient={'linear(to-bl,rgb(30 41 59),rgb(41 37 36))'} rounded={'4rem'}>
-  <Flex
-
+     gap={6} _hover={{borderColor:'blue'}} position={'relative'} border={1} bottom={2} right={1} width={'98vw'}  direction={'column'}   justifyContent={'center'} height={'96vh'} backdropFilter={'blur(4px)'} bgGradient={'linear(to-bl,rgb(30 41 59),rgb(41 37 36))'} rounded={'4rem'}>
+  <Flex 
    direction={'column'} justifyContent={'center'} alignItems={'center'}>
    <Image objectFit={'cover'} src={me} w={64} height={64} rounded={'4rem'}/>
     <Text fontSize={'5xl'} fontStyle={'light'} color={'white'} fontWeight={'bold'}>LOVEBIN JOSEPH ROBIN</Text>
